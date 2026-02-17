06b06df727f64686d97235df66e8a292189121c8
amisadman <inboxsadman@gmail.com>
2026-02-17 17:45:13 +0600

    feat : added getRefreshToken method

--- src/app/utils/token.ts
+++ src/app/utils/token.ts
@@ -2,12 +2,20 @@ import { JwtPayload, SignOptions } from "jsonwebtoken";
 import { jwtUtils } from "./jwt";
 import { envVars } from "../config";
 
-const getAccessToken = (payload : JwtPayload) =>{
-    const accessToken = jwtUtils.createToken(
-        payload,
-        envVars.ACCESS_TOKEN_SECRET,
-        { expiresIn: envVars.ACCESS_TOKEN_EXPIRATION } as SignOptions
-      );
-      return accessToken;
-}
+const getAccessToken = (payload: JwtPayload) => {
+  const accessToken = jwtUtils.createToken(
+    payload,
+    envVars.ACCESS_TOKEN_SECRET,
+    { expiresIn: envVars.ACCESS_TOKEN_EXPIRATION } as SignOptions,
+  );
+  return accessToken;
+};
 
+const getRefreshToken = (payload: JwtPayload) => {
+  const refreshToken = jwtUtils.createToken(
+    payload,
+    envVars.REFRESH_TOKEN_SECRET,
+    { expiresIn: envVars.REFRESH_TOKEN_EXPIRATION } as SignOptions,
+  );
+  return refreshToken;
+};
