3a2374b4e956471cc2b232c358ae4e0654f8c77d
amisadman <inboxsadman@gmail.com>
2026-02-20 04:02:24 +0600

    feat: added manual image upload

--- src/app/config/cloudinary.config.ts
+++ src/app/config/cloudinary.config.ts
@@ -1,4 +1,4 @@
-import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 import { envVars } from ".";
 import AppError from "../errorsHelpers/AppError";
 import status from "http-status";
@@ -8,6 +8,58 @@ cloudinary.config({
   api_secret: envVars.CLOUDINARY_API_SECRET,
   api_key: envVars.CLOUDINARY_API_KEY,
 });
+
+export const uploadFileToCloudinary = async (
+  buffer: Buffer,
+  fileName: string,
+) => {
+  if (!buffer || !fileName) {
+    throw new AppError(status.BAD_REQUEST, "File or filename not found");
+  }
+
+  const extension = fileName.split(".").pop()?.toLocaleLowerCase();
+
+  const fileNameWithoutExtension = fileName
+    .split(".")
+    .slice(0, -1)
+    .join(".")
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    // eslint-disable-next-line no-useless-escape
+    .replace(/[^a-z0-9\-]/g, "");
+
+  const uniqueName =
+    Math.random().toString(36).substring(2) +
+    "-" +
+    Date.now() +
+    "-" +
+    fileNameWithoutExtension;
+
+  const folder = extension === "pdf" ? "pdfs" : "images";
+
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto",
+          public_id: `easyCare/${folder}/${fileName}`,
+          folder: `easyCare/${folder}`,
+        },
+        (error, result) => {
+          if (error) {
+            return reject(
+              new AppError(
+                status.INTERNAL_SERVER_ERROR,
+                "Failed to upload file to Cloudinary",
+              ),
+            );
+          }
+          resolve(result as UploadApiResponse);
+        },
+      )
+      .end(buffer);
+  });
+};
 
 export const deleteFileFromCloudinary = async (url: string) => {
   try {
@@ -20,6 +72,7 @@ export const deleteFileFromCloudinary = async (url: string) => {
       await cloudinary.uploader.destroy(publicId, {
         resource_type: "image",
       });
+      console.log(`File ${publicId} deleted from cloudinary`);
     }
   } catch (error) {
     console.log("Error Deleting file from cloudinary");
